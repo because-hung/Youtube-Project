2a0aa2085c8a8c13a3ab0df9fdbc7415d564823a
Ying Wei hung <4a33a006@stust.edu.tw>
2021-10-12 02:37:01 +0800

    heroku set

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'yt-project',
+    title: 'youtube',
     htmlAttrs: {
       lang: 'en'
     },
